3fae451410dc0e3d827a9970e3a18cbe44302db5
nwmcv <65606594+nwmcv@users.noreply.github.com>
2021-02-12 19:24:19 +0100

    Add files via upload

--- fonctions.py
+++ fonctions.py
@@ -4,8 +4,7 @@ import sqlite3
 def connexion_bd(bd_path):
     """
     Fonction: connexion_bd
-    : Parametre : bd_path #Chemin d'accès vers la base de données
-    :  Renvoie  : la connexion à la base de données
+    : parametre : bd_path #Chemin d'accès vers la base de données
     """
     connexion = None
     try:
@@ -16,16 +15,30 @@ def connexion_bd(bd_path):
     return connexion
 
 def execute_sql(connexion,sql):
-    """
-    Fonction: execute_sql
-    : Parametres : connexion -> (connexion à une base de données avec sqlite3), sql -> (requete SQL au format str)
-    :   Renvoie  : Affiche les différentes lignes de la requete
-    """
     cur = connexion.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
+def question(req):
+    q=req.split('\n')
+    return q[0]
+
+def requete(req):
+    q=req.split('\n')
+    return q[1]
+
+def question_requete(req):
+    q=req.split('\n')
+    return q[0],q[1]
+
+def txt_requete(req_path):
+    req=open(req_path)
+    txt=req.read()
+    req.close()
+    return txt
+
+#Traduction des fonctions:
 database_connexion = connexion_bd
 run_sql = execute_sql
