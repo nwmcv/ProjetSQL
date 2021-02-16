39c569f97d41db3062b1492dc3818190b01dab4e
nwmcv <65606594+nwmcv@users.noreply.github.com>
2021-02-16 14:24:47 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,84 @@
-from fonctions_base_de_donnees import *
+from fonctions import *
+import os
+
+def Menu():
+    action=int(input('Que voulez Vous faire ?: \n --1/Executer une requète \n --2/Ajouter une requète \n 3/Exit \n :'))
+    if action not in [1,2,3,4,5]:
+        print("la valeur entrés n'est pas valide\n")
+        Menu()
+    elif action == 1:
+        Executer()
+    elif action == 2:
+        Ajouter()
+    elif action == 3:
+        modifier()
+    elif action == 4:
+        supprimer()
+    elif action == 5:
+        print('Sortie ...')
+
+def Executer():
+    req_a_executer = {}
+    liste_choix_req=choix_req()
+    error,req_error=req_existe(liste_choix_req)
+    if error==True:
+        print("\n la requete ",req_error," n'existe pas\n")
+        return Executer()
+    else:
+        print('OK')
+
+def Ajouter():
+    pass
+
+def modifier():
+    pass
+
+def supprimer():
+    pass
+
+def choix_req():
+    print("quels questions choisissez vous ?\n")
+    alire = open(fichier_req)
+    texte_req = alire.read()
+    alire.close()
+    questions_req = questions(texte_req)
+    for q in questions_req:
+        print(q)
+    num_q = str(input("veuillez saisir le numéro des questions choisies separé d'un espace : "))
+    num_q = num_q.split(' ')
+    liste_choix=[]
+    for i in range(len(num_q)):
+        try:
+            int(num_q[i])
+            liste_choix.append(num_q[i])
+        except ValueError:
+            print("\n Les valeurs entrées doivent être des entiers !\n")
+            input("taper entrer pour continuer : ")
+            return choix_req()
+    return num_q
+
+def questions(alire):
+    alire=alire.split('#')
+    questions=[]
+    for i in range(1,len(alire)):
+        q=alire[i].split('\n')
+        questions.append(q[0])
+    return questions
+
+def req_existe(list_num):
+    liste_req=os.listdir('requetes')
+    req=''
+    for i in range(len(list_num)):
+        req=nom_req+str(list_num[i])+'.sql'
+        if req not in liste_req:
+            return True,liste_req[i]
+    return False,None
+
+
+nom_req='req'
+fichier_req='requetes/alire.md'
+
+
 
 if __name__ == '__main__':
-    txt=txt_requete('requetes/req1.sql')
-    question,req = question_requete(txt)
-    print(question)
-    print('~~~~~~~~~~~~~~~~~~~~~~~')
-    print(req)
+    Menu()
